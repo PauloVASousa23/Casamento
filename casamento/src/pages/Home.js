bdbc68cb9341621648b2c5dc86d964e493e75c19
Paulo Victor <paulinho_vtr@live.com>
2020-12-12 23:54:46 -0300

    Criado estrutura inicial

---
+++ casamento/src/pages/Home.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import '../assets/Home.css';
+
+function Home() {
+  return (
+    <div>
+      <h3 class="text-center mt-4">Save the Date</h3>
+      <h1 class="text-center">Paulo e Livia</h1>
+      <br/>
+
+      <p class="text-center">Convidamos você para o nosso casamento</p>
+      <div class="container-dia-info">
+        <div>
+          <p>Sábado</p>
+          <p>18:00 PM</p>
+        </div>
+        <div class="divider">
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+        </div>
+        <div>
+          <p>&nbsp;</p>
+          <p>25</p>
+        </div>
+        <div class="divider">
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+        </div>
+        <div>
+          <p>Setembro</p>
+          <p>2021</p>
+        </div>
+      </div>
+      <p class="font-2 text-center mt-4">MORAIS BUFFET/EVENTOS</p>
+      <h5 class="font-3 text-center mt-1">R. dos Macucos, 14 - Parque dos Passaros, São Bernardo do Campo - SP, 09861-350</h5>
+      
+      <div class="container-dia">
+        <div>
+          xx
+        </div>
+        <div>
+          xx
+        </div>
+        <div>
+          xx
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
